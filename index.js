70a380561bac96896b58a5f6ea0015be3c5e79d4
Shashank <shashankkyadari172@gmail.com>
2021-12-27 01:50:27 +0530

    ssd

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const app = express();
+const { append } = require("express/lib/response");
+const { default: axios } = require("axios");
+const key_secret = "MX0O8EfhsKqGR6NgTLc4daMX";
+const key_id = "rzp_test_V0bUr4809xcQba";
+
+const instance = new Razorpay({
+  key_id,
+  key_secret,
+});
+app.use(cors());
+app.use(express.json());
+let resort;
+app.get("/order/:payid", async (req, res) => {
+  const payid = req.params;
+  console.log(payid);
+  resort = await axios.get(
+    `https://candata.herokuapp.com/paymentorder?uniqueid=${payid.payid}`
+  );
+  resort = resort.data;
+  const amount = resort[0].sum * 100;
+  console.log(resort[0].sum);
+  const currency = "INR";
+  const receipt = "bla";
+
+  instance.orders.create({ amount, currency, receipt }, (error, order) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    console.log(order);
+    return res.status(200).json(order);
+  });
+});
+
+app.listen(8000, () => {
+  console.log("listening on" + 8000);
+});
